8c37c755eb7e9617d2a8c5ee5f31934347a44def
nibogorak <nibogorakennyariel@gmail.com>
2023-06-21 10:34:41 +0200

    initialize project

---
+++ JavaScript/Students.js
@@ -0,0 +1,105 @@
+// window.document.getElementById("heading2").style.display="none";
+// document.getElementById("firstname").style.color ="red"
+// document.getElementById("firstname").style.fontWeight ="900"
+
+// let firstName = "Niwemwiza";
+// let number1 = 900;
+// let number2 = 2000;
+// let sum = number1 + number2;
+// console.log(firstName);
+// console.log("The first Number is ", number1, "Second Number is ", number2,
+// "the sum of those 2 numbers are ",sum );
+// Naming conversion
+//If statment
+//loop statment
+//DOM Manipulation
+//Event
+
+// Naming conversion
+// let %number1 = 200;
+// let first name = 300;
+// let _number3 = 1000;
+// let 23number6 = 200;
+// let number300 = 400;
+// let firstname = 300;
+// let firstName = 800;
+// let first_name = 900;
+
+//If statment
+// let ages = 20;
+// How If statment Works
+// if(ages < 20){
+//     console.log("you are steal young")
+// }
+// How If-else statment Works
+// let ages = 20;
+// if(ages<20){
+//     console.log("You are steal young")
+// }else{
+//     console.log("you are an adult!!🥰🥰🥰🥰🥰🥰")
+// }
+
+
+// How Nested If statment Works
+
+// let ages = 20;
+// if(ages < 20){
+//     console.log("you are steal young")
+// }else if(ages == 20){
+//     console.log("Welcome to the show!!")
+// }else{
+//     console.log("You are an adult")
+// }
+//  let firstName = "20";
+//  let isLogedIn = true; 
+//  let number2 = 800;
+
+//  console.log(typeof(firstName))
+//  console.log(typeof(isLogedIn))
+//  console.log(typeof(number2))
+
+//  if(number == 20){
+//     console.log("your number is equal to ", number1);
+//  }
+
+
+// let number1 = 20;
+// if(number1 === 20){
+//     console.log("Your Number is ", number1, "Its type is : ", typeof(number1));
+// }else{
+//     console.log("the types does not match")
+// }
+
+// let number1 = 2
+// let number2 = "50"
+// let firstName ="Muheto"
+// let lastName = "Hodal"
+// let fullName = firstName+"  " +lastName
+// let sum = number1 + number2
+// console.log("The sum is ", sum)
+// console.log("Your Names are : ",fullName);
+
+//How Loops Works
+
+
+// for(let number1=0; number1<=10 ; number1++ ){
+//     console.log(number1)
+// }
+// let i=0;
+// for(i=1; i<100; i=i+20){
+//     if(i==61){
+//         console.log("Nakubonye");
+//     }
+//     console.log(i);
+// }
+// console.log("Finished")
+
+//DOM Manipulation
+let number = 90;
+window.document.getElementById("studentNo").style.background = "orange";
+window.document.getElementById("studentNo").style.color = "white";
+// document.getElementById("studentNo").innerHTML = "300000000"
+if(number<100){
+    document.getElementById("studentNo").innerHTML ="you are steal young"
+}
+
---
+++ JavaScript/student.js
@@ -0,0 +1,47 @@
+//window.document.getElementById("heading2").style.display="none"
+//document.getElementById("firtsname").style.color="red"
+//document.getElementById("firtsname").style.fontWeight="900"
+
+//let firstname ="Niwemwiza"
+//let number1=900;
+//let number2=2000
+//let sum=number1 + number2;
+//console.log("the first number is",number1, "second number is",number2, "the sum of those 2 numbers are",sum)
+//console.log(firstname);
+//Naming conversion
+//If statment
+
+
+//Dom Manipulation
+//event
+
+//Naming conversion
+//let %number1 = 200;
+//let firstName = 900;
+//let first_name = 800;
+
+
+//If statment
+//let ages = 20;
+
+//if(ages < 20){
+    //console.log("you are steal young")
+//}
+
+// How If-else statment works
+//if(ages<20){
+  //  console.log("you are steal young")
+//}else{
+  //  console.log("you are an adult!!😀😀")
+//}
+
+
+//How Nested If statment Works
+ //if(ages < 20){
+   // console.log("you are steal young") 
+//} else if(ages==20){
+  //  console.log("Welcome to the show!!")
+//}else{
+  //  console.log("you are an adult!!😀😀")
+//}
+    
